85f3c638073e383b7bede1cbcd9bbcca288dd2b5
user <user@DESKTOP-COJ9858>
2022-06-17 16:26:29 +0900

    111

--- GitProject/src/com/sist/MainClass.java
+++ GitProject/src/com/sist/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github~~");
+		System.out.println("Hello Github~~");
 		//형상관리
 
 	}
