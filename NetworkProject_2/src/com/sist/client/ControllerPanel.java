90b388b1444f4355702b0286f3d6a9e9c69c2e78
user <user@DESKTOP-COJ9858>
2022-06-23 11:09:08 +0900

    1111

---
+++ NetworkProject_2/src/com/sist/client/ControllerPanel.java
@@ -0,0 +1,12 @@
+package com.sist.client;
+
+import java.awt.Color;
+import javax.swing.*;
+
+public class ControllerPanel extends JPanel{
+	public ControllerPanel()
+	{
+		setBackground(Color.yellow);
+	}
+
+}
---
+++ NetworkProject_2/src/com/sist/main/NetworkMain.java
@@ -0,0 +1,38 @@
+package com.sist.main;
+import com.sist.client.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NetworkMain extends JFrame{
+	MenuForm menu=new MenuForm();
+	ControllerPanel cp=new ControllerPanel();
+	WaitForm wr=new WaitForm();
+	public NetworkMain()
+	{
+		setTitle("레시피 프로그램");
+		setLayout(null); //사용자 정의 (직접 배치)
+		menu.setBounds(10, 15, 100, 350);
+		add(menu);
+		
+		cp.setBounds(120,15,850,820);
+		add(cp);
+		
+		wr.setBounds(980, 15, 250, 700);
+		add(wr);
+		
+		setSize(1250,900);
+		setVisible(true);
+		//종료
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
+		}catch(Exception ex) {}
+		new NetworkMain();
+	}
+
+}
